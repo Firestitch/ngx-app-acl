3183c1a540793bd8eb8d6243195ff32baa58db05
Raymond Gigliotti <ray@firestitch.com>
2020-07-28 07:43:32 -0400

    updated kitchen sink example data

--- src/app/components/acl-object-roles/acl-object-roles.component.ts
+++ src/app/components/acl-object-roles/acl-object-roles.component.ts
@@ -24,4 +24,8 @@ export class FsAclObjectRolesComponent {
   public compareAclRole = (o1: any, o2: any) => {
     return o1 && o2 && o1.id === o2.id;
   }
+
+  public changed() {
+    this.change.emit(this.aclObjectRoles);
+  }
 }
